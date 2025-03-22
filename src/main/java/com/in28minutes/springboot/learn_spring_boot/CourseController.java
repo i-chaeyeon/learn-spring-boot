6e83a070a84b0e527620e66ac5267ee32d3e5658
i-chaeyeon <leechae0605@naver.com>
2025-03-22 18:52:15 +0900

    250322 섹션 5 - 8단계 - Spring Boot DevTools로 빠르게 빌드하기

---
+++ src/main/java/com/in28minutes/springboot/learn_spring_boot/CourseController.java
@@ -0,0 +1,24 @@
+package com.in28minutes.springboot.learn_spring_boot;
+
+// create a simple REST API with URL/courses
+// when somebody hits the URL, return this data ...
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    // /courses
+    @RequestMapping("/courses")
+    // Course: id, name, author
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28minutes"),
+                new Course(2, "Learn DevOps", "in28minutes")
+        );
+    }
+}
